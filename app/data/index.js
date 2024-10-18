323a901ae837ee220c41abd14bdc43744c36e540
Rana Salem <ranatasalem@gmail.com>
2024-10-18 15:46:32 +0100

    wip: tests for /data/index.js

--- app/data/index.js
+++ app/data/index.js
@@ -9,17 +9,17 @@ const dirname = path.dirname(filename)
 const modelPath = path.join(dirname, 'models')
 
 const db = {}
-
 const sequelize = new Sequelize(databaseConfig.database, databaseConfig.username, databaseConfig.password, databaseConfig)
 
-fs.readdirSync(modelPath)
+const files = fs.readdirSync(modelPath)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) === '.js')
   })
-  .forEach(file => {
-    const model = require(path.join(modelPath, file))(sequelize, DataTypes)
-    db[model.name] = model
-  })
+
+for (const file of files) {
+  const model = (await import(path.join(modelPath, file))).default(sequelize, DataTypes)
+  db[model.name] = model
+}
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
