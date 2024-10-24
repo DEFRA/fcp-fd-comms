1780b555669c0eaf6a6655a0cf56b956016233e9
Rana Salem <ranatasalem@gmail.com>
2024-10-24 14:32:09 +0100

    unit testing for service bus config in non-prd env

--- test/unit/config/message.test.js
+++ test/unit/config/message.test.js
@@ -29,8 +29,6 @@ describe('Service Bus Configuration', () => {
 
     const { messageConfig } = await import('../../../app/config/index.js')
 
-    console.log(messageConfig)
-
     expect(messageConfig).toEqual({
       host: 'mock-prd-message-host',
       username: 'mock-prd-user',
@@ -43,4 +41,32 @@ describe('Service Bus Configuration', () => {
       type: 'subscription'
     })
   })
+
+  test('should validate configuration correctly in non-production environment without managed identity', async () => {
+    process.env.NODE_ENV = environments.DEVELOPMENT
+    process.env.MESSAGE_HOST = 'mock-dev-message-host'
+    process.env.MESSAGE_USER = 'mock-dev-user'
+    process.env.MESSAGE_PASSWORD = 'mock-dev-password'
+    process.env.COMMS_AZURE_CLIENT_ID = 'mock-dev-client-id'
+    process.env.MESSAGES_SUBSCRIPTION_ADDRESS = 'mock-dev-subscription-address'
+    process.env.MESSAGES_TOPIC_ADDRESS = 'mock-dev-topic-address'
+
+    const { messageConfig } = await import('../../../app/config/index.js')
+
+    console.log(messageConfig)
+
+    expect(messageConfig).toEqual({
+      host: 'mock-dev-message-host',
+      username: 'mock-dev-user',
+      password: 'mock-dev-password',
+      useCredentialChain: false,
+      managedIdentityClientId: 'mock-dev-client-id',
+      appInsights: undefined,
+      address: 'mock-dev-subscription-address',
+      topic: 'mock-dev-topic-address',
+      type: 'subscription'
+    })
+  })
 })
+
+
