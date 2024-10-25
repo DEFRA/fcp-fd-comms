02fc34c48c6d0b141c1095229113a0212ca54c8f
Rana Salem <ranatasalem@gmail.com>
2024-10-25 13:35:39 +0100

    update version

--- app/messages/index.js
+++ app/messages/index.js
@@ -3,15 +3,6 @@ import { messageConfig } from '../config/index.js'
 import { MessageReceiver } from 'ffc-messaging'
 import { farmerApply } from './farmer-apply.js'
 
-const handleMessage = async (message, receiver) => {
-  try {
-    await farmerApply(message)
-    await receiver.completeMessage(message)
-  } catch (error) {
-    throw new Error('Message error', util.inspect(error.message, false, null, true))
-  }
-}
-
 const startMessaging = async () => {
   let commsReceiver // eslint-disable-line
   const receiverAction = (message) => handleMessage(message, commsReceiver)
@@ -21,6 +12,15 @@ const startMessaging = async () => {
   )
   await commsReceiver.subscribe()
   console.info('fcp-fd-comms is ready to consume messages')
+}
+
+const handleMessage = async (message, receiver) => {
+  try {
+    await farmerApply(message)
+    await receiver.completeMessage(message)
+  } catch (error) {
+    throw new Error('Message error', util.inspect(error.message, false, null, true))
+  }
 }
 
 export { startMessaging }
