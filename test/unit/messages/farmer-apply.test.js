688b031c5b39f47b75bd850570d4159a901c31f7
Rana Salem <ranatasalem@gmail.com>
2024-10-25 10:30:10 +0100

    unit test coverage for farmerApply

--- test/unit/messages/farmer-apply.test.js
+++ test/unit/messages/farmer-apply.test.js
@@ -14,11 +14,8 @@ const { NotifyClient } = await import('notifications-node-client')
 const { farmerApply } = await import('../../../app/messages/farmer-apply.js')
 
 describe('Farmer Apply', () => {
-  process.env.NOTIFY_API_KEY = 'mock-notify-api-key'
-  process.env.CONFIRM_NEW_USER_NOTIFY_TEMPLATE_ID = 'mock-confirm-new-user-notify-template-id'
-  process.env.NOTIFY_TEST_EMAIL = 'mock-email@test.com'
-
   let mockMessage
+  process.env.NOTIFY_API_KEY = 'mock-notify-api-key'
 
   beforeEach(() => {
     mockMessage = {
@@ -39,13 +36,13 @@ describe('Farmer Apply', () => {
 
   test('should call uuid to create a reference', async () => {
     const { v4: uuidv4 } = await import('uuid')
-    await farmerApply(mockMessage) // move to beforeEach block?
+    await farmerApply(mockMessage)
 
     expect(uuidv4).toHaveBeenCalled()
   })
 
   test('should create a new NotifyClient instance with the current Notify API key', async () => {
-    await farmerApply(mockMessage) // move to beforeEach block?
+    await farmerApply(mockMessage)
 
     expect(NotifyClient).toHaveBeenCalledWith('mock-notify-api-key')
   })
