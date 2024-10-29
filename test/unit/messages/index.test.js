119d61029b98c4f60afb0749a41536b6382618f6
Rana Salem <ranatasalem@gmail.com>
2024-10-29 16:38:25 +0000

    test coverage: 92%

--- test/unit/messages/index.test.js
+++ test/unit/messages/index.test.js
@@ -18,17 +18,4 @@ describe('Start Messaging', () => {
     expect(MessageReceiver).toHaveBeenCalled()
     expect(MessageReceiver.mock.calls[0][1]).toBeInstanceOf(Function)
   })
-
-  // test('should call handleMessage when a message is received', async () => {
-  //   const { MessageReceiver } = await import('ffc-messaging')
-  //   const { startMessaging } = await import('../../../app/messages/index.js')
-  //   const { handleMessage } = await import('../../../app/messages/handle-message.js')
-  //   const message = { id: 1, content: 'mock content' }
-
-  //   await startMessaging()
-  //   const receiverAction = MessageReceiver.mock.calls[0][1]
-  //   await receiverAction(message)
-
-  //   expect(handleMessage).toHaveBeenCalledWith(message, expect.any(Object))
-  // })
 })
