a885af4267228a060b0d254e3423c86d0e29aef2
Rana Salem <ranatasalem@gmail.com>
2024-10-29 23:00:35 +0000

    revert back to previous name for config export

--- app/config/message.js
+++ app/config/message.js
@@ -1,7 +1,7 @@
 import convict from 'convict'
 import environments from '../constants/environments.js'
 
-const messageConfig = convict({
+const config = convict({
   messageQueue: {
     host: {
       doc: 'Message queue host.',
@@ -62,6 +62,6 @@ const messageConfig = convict({
   }
 })
 
-messageConfig.validate({ allowed: 'strict' })
+config.validate({ allowed: 'strict' })
 
-export default messageConfig
+export default config
