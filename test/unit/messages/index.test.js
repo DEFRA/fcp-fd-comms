5c8d9a4422b3fb7f94245dd7911183154a9b9d12
Rana Salem <ranatasalem@gmail.com>
2024-10-29 16:28:30 +0000

    update docker compose var names

--- test/unit/messages/index.test.js
+++ test/unit/messages/index.test.js
@@ -19,16 +19,16 @@ describe('Start Messaging', () => {
     expect(MessageReceiver.mock.calls[0][1]).toBeInstanceOf(Function)
   })
 
-  // test('should call handleMessage when a message is received', async () => {
-  //   const { MessageReceiver } = await import('ffc-messaging')
-  //   const { startMessaging } = await import('../../../app/messages/index.js')
-  //   const { handleMessage } = await import('../../../app/messages/handle-message.js')
-  //   const message = { id: 1, content: 'mock content' }
+  test('should call handleMessage when a message is received', async () => {
+    const { MessageReceiver } = await import('ffc-messaging')
+    const { startMessaging } = await import('../../../app/messages/index.js')
+    const { handleMessage } = await import('../../../app/messages/handle-message.js')
+    const message = { id: 1, content: 'mock content' }
 
-  //   await startMessaging()
-  //   const receiverAction = MessageReceiver.mock.calls[0][1]
-  //   await receiverAction(message)
+    await startMessaging()
+    const receiverAction = MessageReceiver.mock.calls[0][1]
+    await receiverAction(message)
 
-  //   expect(handleMessage).toHaveBeenCalledWith(message, expect.any(Object))
-  // })
+    expect(handleMessage).toHaveBeenCalledWith(message, expect.any(Object))
+  })
 })
