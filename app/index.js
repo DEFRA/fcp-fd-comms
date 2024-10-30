23c1fb97196a2f64d65a80ca9cf2d536841af0cf
Rana Salem <ranatasalem@gmail.com>
2024-10-30 10:17:33 +0000

    remove server/routes & associated integration tests

--- app/index.js
+++ app/index.js
@@ -1,11 +1,8 @@
 import { setup } from './insights.js'
 import 'log-timestamp'
-import { createServer } from './server.js'
 import { startMessaging } from './messages/index.js'
 
 const init = async () => {
-  const server = await createServer()
-  await server.start()
   await startMessaging()
   console.log('fcp-fd-comms is ready')
 }
