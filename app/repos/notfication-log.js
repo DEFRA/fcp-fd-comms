448950d102af6a1ecc889d29abd1069de6977af3
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-10-31 14:29:04 +0000

    Move mock data into module

--- app/repos/notfication-log.js
+++ app/repos/notfication-log.js
@@ -1,24 +1,7 @@
-const mockData = [
-  {
-    id: 'eb12be08-91aa-4bc2-8e4a-1b95214da178',
-    status: 'sending'
-  },
-  {
-    id: '127742b6-c705-4fc3-be1d-6a193e83490e',
-    status: 'sending'
-  },
-  {
-    id: 'c3f864f3-1e37-4a1b-834b-e01b4349d380',
-    status: 'created'
-  },
-  {
-    id: '95f281fd-dbc2-4ec0-a33d-ff5e7cc25244',
-    status: 'delivered'
-  }
-]
+import data from '../constants/mock-notification-log.js'
 
 const getPendingNotifications = () => {
-  const notifcations = mockData
+  const notifcations = data
     .filter(n =>
       n.status === 'sending' ||
       n.status === 'created'
@@ -28,7 +11,7 @@ const getPendingNotifications = () => {
 }
 
 const updateNotificationStatus = (notification, status) => {
-  const org = mockData.find(({ id }) => id === notification.id)
+  const org = data.find(({ id }) => id === notification.id)
 
   if (!org) {
     console.log(`Notification ${notification.id} not found in data`)
