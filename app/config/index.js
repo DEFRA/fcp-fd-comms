3ab2db41e1e91149b68fb413a038f3648f797bd0
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-11-04 09:05:52 +0000

    Remove missed merge markers

--- app/config/index.js
+++ app/config/index.js
@@ -1,15 +1,9 @@
 import messageConfig from './messaging.js'
-<<<<<<< HEAD
 import jobsConfig from './jobs.js'
-=======
->>>>>>> main
 import notifyConfig from './notify.js'
 
 export {
   messageConfig,
-<<<<<<< HEAD
   jobsConfig,
-=======
->>>>>>> main
   notifyConfig
 }
