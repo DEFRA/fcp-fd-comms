3e0c3277e5480356b40aaed7387e8d005e82df44
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-11-12 12:32:48 +0000

    Use test.each for update tests

--- test/unit/repos/notification-log.test.js
+++ test/unit/repos/notification-log.test.js
@@ -1,10 +1,13 @@
 import { jest } from '@jest/globals'
+
 const mockCreate = jest.fn()
+const mockFindOne = jest.fn()
 
 jest.unstable_mockModule('../../../app/data/index.js', () => ({
   default: {
     notifyApiRequestSuccess: {
-      create: mockCreate
+      create: mockCreate,
+      findOne: mockFindOne
     },
     notifyApiRequestFailure: {
       create: mockCreate
@@ -12,9 +15,17 @@ jest.unstable_mockModule('../../../app/data/index.js', () => ({
   }
 }))
 
-const { logCreatedNotification, logRejectedNotification } = await import('../../../app/repos/notification-log.js')
+const {
+  logCreatedNotification,
+  logRejectedNotification,
+  updateNotificationStatus
+} = await import('../../../app/repos/notification-log.js')
 
 describe('Notification Log Repository', () => {
+  beforeAll(() => {
+    jest.useFakeTimers('modern')
+  })
+
   beforeEach(() => {
     jest.clearAllMocks()
   })
@@ -45,4 +56,42 @@ describe('Notification Log Repository', () => {
       error: 'test error'
     })
   })
+
+  test.each(['created', 'sending'])(
+    'should not complete notification if status is %s',
+    async (status) => {
+      const notification = {
+        save: jest.fn(),
+        status: 'created',
+        statusUpdatedAt: new Date(),
+        completed: null
+      }
+
+      mockFindOne.mockResolvedValue(notification)
+
+      await updateNotificationStatus('f824cbfa-f75c-40bb-8407-8edb0cc469d3', status)
+
+      expect(notification.completed).toBeNull()
+    }
+  )
+
+  test.each(['delivered', 'permanent-failure', 'temporary-failure', 'technical-failure'])(
+    'should complete notification if status is %s',
+    async (status) => {
+      jest.setSystemTime(new Date('2024-01-01T15:00:00.000Z'))
+
+      const notification = {
+        save: jest.fn(),
+        status: 'created',
+        statusUpdatedAt: new Date(),
+        completed: null
+      }
+
+      mockFindOne.mockResolvedValue(notification)
+
+      await updateNotificationStatus('f824cbfa-f75c-40bb-8407-8edb0cc469d3', status)
+
+      expect(notification.completed).toEqual(new Date('2024-01-01T15:00:00.000Z'))
+    }
+  )
 })
