66c3be678dcd3a87fae18aaeef22989a75353abe
Rana Salem <ranatasalem@gmail.com>
2024-12-04 16:09:27 +0000

    update switch case & reference to type

--- app/messages/inbound/handle-message.js
+++ app/messages/inbound/handle-message.js
@@ -3,16 +3,19 @@ import { sendNotificationV2 } from './send-notification-v2.js'
 
 const getVersionHandler = (type) => {
   switch (type) {
-    case 'uk.gov.fcp.sfd.notificaiton.request.v2':
+    case 'uk.gov.fcp.sfd.notification.request.v2':
+    case 'uk.gov.fcp.sfd.notification.request':
       return sendNotificationV2
-    default:
+    case 'uk.gov.fcp.sfd.notification.request.v3':
       return sendNotification
+    default:
+      throw new Error(`Unsupported message type: ${type}`)
   }
 }
 
 const handleMessage = async (message, receiver) => {
   try {
-    const versionHandler = getVersionHandler(message.type)
+    const versionHandler = getVersionHandler(message.body.type)
     await versionHandler(message.body)
     await receiver.completeMessage(message)
   } catch (error) {
