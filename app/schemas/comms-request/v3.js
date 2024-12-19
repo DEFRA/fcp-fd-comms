c0f6e5aac898f2a59af72d62c505783c5c5bd86f
Shaun Fitzsimons <87677173+sfitz42@users.noreply.github.com>
2024-12-19 10:21:44 +0000

    SFDP-36 - Malformed Message Bug Fixes (#26)
    * Fix message v3 schema rules
    
    * Add regex to sourceSystem matching v3.3.0 AsyncAPI contract
    * Add missing .required() to top-level Joi object
    * Override default object error message
    
    * Use Joi label rather than message override
    
    * Add v3 schema tests

--- app/schemas/comms-request/v3.js
+++ app/schemas/comms-request/v3.js
@@ -3,7 +3,7 @@ import Joi from 'joi'
 import { sbi, crn } from '../common/index.js'
 
 const v3 = Joi.object({
-  id: Joi.string().required(),
+  id: Joi.string().uuid().required(),
   source: Joi.string().required(),
   specversion: Joi.string().required(),
   type: Joi.string().required(),
@@ -12,18 +12,18 @@ const v3 = Joi.object({
   data: Joi.object({
     crn: crn.optional(),
     sbi: sbi.required(),
-    sourceSystem: Joi.string().required(),
+    sourceSystem: Joi.string().regex(/^[a-z0-9-_]+$/).required(),
     notifyTemplateId: Joi.string().uuid().required(),
     commsType: Joi.string().valid('email').required(),
-    commsAddresses: Joi.alternatives(
-      Joi.array().items(Joi.string().email().required()).min(1),
-      Joi.string().email().required()
-    ),
+    commsAddresses: Joi.alternatives().conditional(Joi.array(), {
+      then: Joi.array().items(Joi.string().email()).min(1).max(10).required(),
+      otherwise: Joi.string().email().required()
+    }).required(),
     personalisation: Joi.object().unknown().required(),
     reference: Joi.string().required(),
     oneClickUnsubscribeUrl: Joi.string().uri().optional(),
     emailReplyToId: Joi.string().uuid().required()
   }).required()
-})
+}).label('body').required()
 
 export default v3
