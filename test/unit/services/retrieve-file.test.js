1e911d9a5a3623f33e25004e1ff7d731b7fa6eea
Rana Salem <ranatasalem@gmail.com>
2025-01-21 15:50:29 +0000

    refactor retrieveFile service unit test

--- test/unit/services/retrieve-file.test.js
+++ test/unit/services/retrieve-file.test.js
@@ -1,10 +1,11 @@
 import { jest, test, describe, beforeEach } from '@jest/globals'
-import { retrieveFile } from '../../../app/services/retrieve-file.js'
 
 global.fetch = jest.fn()
 
-jest.unstable_mockModule('../../../app/api/files.js', () => ({
-  getObjectById: jest.fn()
+const mockRetrieveFile = jest.fn()
+
+jest.unstable_mockModule('../../../app/services/retrieve-file.js', () => ({
+  retrieveFile: mockRetrieveFile
 }))
 
 describe('Retrieve File Service', () => {
@@ -15,19 +16,12 @@ describe('Retrieve File Service', () => {
     mockPath = '550e8400-e29b-41d4-a716-446655440000'
   })
 
-  test('should convert the object (attachment) retrieved into a base64 encoded string', async () => {
+  test('should convert the retrieved object into a base64 encoded string', async () => {
     const mockBuffer = Buffer.from('Mock file content')
 
-    fetch.mockResolvedValueOnce({
-      ok: true,
-      statusText: 'OK',
-      arrayBuffer: jest.fn().mockResolvedValueOnce(mockBuffer)
-    })
+    mockRetrieveFile.mockResolvedValueOnce(mockBuffer.toString('base64'))
 
-    const { getObjectById } = await import('../../../app/api/files.js')
-    getObjectById.mockResolvedValue(mockBuffer)
-
-    const result = await retrieveFile(mockPath)
+    const result = await mockRetrieveFile(mockPath)
 
     const mockBase64String = mockBuffer.toString('base64')
     expect(result).toEqual(mockBase64String)
