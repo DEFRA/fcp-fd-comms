88d0c1150c0b7f1c55d0333342a286f5166ab10c
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-02-13 12:07:05 +0000

    Temp disable message validation

--- app/messages/inbound/comms-request/handler.js
+++ app/messages/inbound/comms-request/handler.js
@@ -1,32 +1,11 @@
-import { validate } from '../../../schemas/validate.js'
-import {
-  v3 as commsSchema
-} from '../../../schemas/comms-request/index.js'
-import { publishInvalidRequest, publishReceived } from '../../outbound/notification-status/publish.js'
 import { sendNotification } from './send-notification.js'
 
 const handleCommsRequest = async (message, receiver) => {
   const commsRequest = message.body
 
   try {
-    const [validated, errors] = await validate(commsSchema, commsRequest)
-
-    if (errors) {
-      console.error('Invalid comms request received. Request ID:', commsRequest.id)
-
-      if (commsRequest.id) {
-        await publishInvalidRequest(commsRequest, errors)
-      } else {
-        console.error('No ID provided in message. Cannot publish invalid request to data layer.')
-      }
-
-      await receiver.deadLetterMessage(message)
-
-      return
-    }
-
-    await publishReceived(validated)
-    await sendNotification(validated)
+    await publishReceived(commsRequest)
+    await sendNotification(commsRequest)
 
     await receiver.completeMessage(message)
   } catch (error) {
