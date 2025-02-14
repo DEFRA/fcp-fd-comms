43b382e30ea80daa550b8ba80060fac0ae5a783e
jungabunga <amir.ishmuhametov@gmail.com>
2025-02-14 09:14:51 +0000

    added joi validation for test emails pattern

--- app/schemas/comms-request/v3.js
+++ app/schemas/comms-request/v3.js
@@ -1,6 +1,16 @@
 import Joi from 'joi'
-
 import { sbi, crn } from '../common/index.js'
+import environments from '../../constants/environments.js'
+
+const validateTestEmail = [
+  environments.DEVELOPMENT,
+  environments.TEST
+].includes(process.env.NODE_ENV)
+  ? Joi.alternatives().try(
+    Joi.string().email(),
+    Joi.string().valid('temp-fail@simulator.notify', 'perm-fail@simulator.notify')
+  )
+  : Joi.string().email()
 
 const v3 = Joi.object({
   id: Joi.string().uuid().required(),
@@ -16,8 +26,8 @@ const v3 = Joi.object({
     notifyTemplateId: Joi.string().uuid().required(),
     commsType: Joi.string().valid('email').required(),
     commsAddresses: Joi.alternatives().conditional(Joi.array(), {
-      then: Joi.array().items(Joi.string().email()).min(1).max(10).required(),
-      otherwise: Joi.string().email().required()
+      then: Joi.array().items(validateTestEmail).min(1).max(10).required(),
+      otherwise: validateTestEmail.required()
     }).required(),
     personalisation: Joi.object().unknown().required(),
     reference: Joi.string().required(),
